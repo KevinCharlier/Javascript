5d33326e2c6a18f66250b559ccefcbe61c8c10a2
KevinCharlier <kevcharlier@gmail.com>
2018-04-26 11:16:56 +0200

    cookie

--- drill-level2/cookie/app.js
+++ drill-level2/cookie/app.js
@@ -1,18 +1,25 @@
-let cookiecount = 0
+let score = 0;
 
 let add = () => {
-    cookiecount = cookiecount + 1
-    document.getElementById('text').value = cookiecount
-    document.title = cookiecount + " Cookies"
-}
+  score = score + 1;
+  document.getElementById("main").value = score;
+  document.title = score + " Cookies";
+};
 
 let save = () => {
-    localStorage.setItem("cookiecount", cookiecount)
-}
+  localStorage.setItem("score", score);
+};
 
 let load = () => {
-    cookiecount = localStorage.getItem("cookiecount")
-    cookiecount = parseInt(cookiecount)
-    document.getElementById('text').value = cookiecount
-    document.title = cookiecount + " Cookies"
-}
+  score = localStorage.getItem("score");
+  score = parseInt(score);
+  document.getElementById("main").value = score;
+  document.title = score + " Cookies";
+  affichage.innerHTML = "Vous avez " + score + " cookies";
+};
+
+let affichageScore = () => {
+    affichage.innerHTML = "Vous avez " + score + " cookies";
+  }
+
+
