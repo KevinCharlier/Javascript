dc977e705d1eed711bf7ab7bfcc8d15d7538dcfd
KevinCharlier <kevcharlier@gmail.com>
2018-05-02 16:35:48 +0200

    maj cookie

--- drill-level2/cookie/app.js
+++ drill-level2/cookie/app.js
@@ -19,7 +19,39 @@ let load = () => {
 };
 
 let affichageScore = () => {
-    affichage.innerHTML = "Vous avez " + score + " cookies";
+  affichage.innerHTML = "Vous avez " + score + " cookies";
+}
+
+let multiply = document.getElementById("multiplier");
+let autoclick = document.getElementById("autoclick");
+let bonus = document.getElementById("bonus");
+
+let multiplierCost = 20;
+let autoclickCost = 200;
+let bonusCost = 600;
+let multiplier = 1;
+
+multiply.disabled = true;
+autoclick.disabled = true;
+bonus.disabled = true;
+
+let disabled = () => {
+  if (score > 50) {
+    multiply.disabled = false;
   }
+  if (score > 500) {
+    autoclick.disabled = false;
+  }
+  if (score > 5000) {
+    bonus.disabled = false
+  }
+}
 
+let affichageBoutons = () => {
+  multiply.value = "Multiplier X" + multiplier + " | cost: " + multiplierCost;
+  autoclick.value = "Autoclick | cost: " + autoclickCost;
+  bonus.value = "Bonus | cost: " + bonusCost;
+}
 
+affichageBoutons()
+disabled()
